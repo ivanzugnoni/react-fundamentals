5c6235497adbdac82087c57a1f241c3828686ae2
Ivan Zugnoni <zugnoni.ivan@gmail.com>
2016-04-06 14:55:42 -0300

    React fundamentals - up to video 4

--- app/index.js
+++ app/index.js
@@ -1,64 +1,9 @@
-var USER_DATA = {
-    name: 'Ivan Zugnoni',
-    username: 'ivanzugnoni',
-    image: 'https://avatars1.githubusercontent.com/u/2788551?v=3&s=460'
-}
-
 var React = require('react');
 var ReactDOM = require('react-dom');
+var routes = require('./config/routes');
 
-var ProfilePic = React.createClass({
-    render: function () {
-        return (
-            <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
-        )
-    }
-});
-
-
-var ProfileName = React.createClass({
-    render: function () {
-        return (
-            <div>
-                {this.props.name}
-            </div>
-        )
-    }
-});
-
-var ProfileLink = React.createClass({
-    render: function () {
-        return (
-            <a href={"https://www.github.com/" + this.props.username}>
-                {this.props.username}
-            </a>
-        )
-    }
-});
-
-
-var Avatar = React.createClass({
-    render: function () {
-        return (
-            <div>
-                <ProfilePic imageUrl={this.props.user.image}/>
-                <ProfileName name={this.props.user.name}/>
-                <ProfileLink username={this.props.user.username}/>
-            </div>
-        )
-    }
-});
 
 ReactDOM.render(
-    <Avatar user={USER_DATA} />,
+    routes,
     document.getElementById('app')
 );
-
-
-/* Rules for components:
-    Focused
-    Independent
-    Reusable
-    Small
-    Testable
-*/
